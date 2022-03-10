bd106cb4bea88c80c1f702fc7b7c9d2e43701189
JanaZorigt <91855087+JanaZorigt@users.noreply.github.com>
2022-03-09 22:05:51 -0500

    Changed the font

--- src/About.js
+++ src/About.js
@@ -3,7 +3,7 @@ import React from "react";
 export default function About() {
   return (
     <div className="About">
-      <div className="hi">Hello, I'm</div>
+      <div className="hi">Hi, I'm</div>
       <h1>Jana Zorigt</h1>
       <h3 className="mt-4">Front-end developer, based in the United States</h3>
       <div className="about-paragraph">
