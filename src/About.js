cf4da10cf3850615478a8faa51397b8cdef90ccb
JanaZorigt <91855087+JanaZorigt@users.noreply.github.com>
2022-03-11 11:08:11 -0500

    Added"technologies used"

--- src/About.js
+++ src/About.js
@@ -5,7 +5,7 @@ export default function About() {
     <div className="About">
       <div className="hi">Hi, I'm</div>
       <h1>Jana Zorigt</h1>
-      <h3 className="mt-4">Front-end developer, based in the United States</h3>
+      <h3 className="mt-3">Front-end developer, based in the United States</h3>
       <div className="about-paragraph">
         <p className="mt-5">
           I'm currently seeking new opportunities in front-end development. I'm
--- src/Projects.js
+++ src/Projects.js
@@ -4,24 +4,15 @@ export default function Projects() {
   return (
     <div className="Projects">
       <div className="container">
-        <div className="row mt-5">
-          <div className="col-md-6">
+        <div className="row">
+          <div className="col-sm">
+            <h2>Chicago Architecture</h2>
             <img
               src="images/Architecture.png"
-              alt="Architecture"
+              alt="Architecture project"
               className="architecture-image img-fluid rounded"
             />
-          </div>
-          <div className="col-md-6">
-            <h2>Architecture Landing Page</h2>
-            <p className="mt-3">
-              I entered the world of front-end development by gaining some
-              knowledge in HTML, CSS, and JavaScript. I used my knowledge of
-              these languages, along with my new skills in VS Code, to build a
-              landing page about Architecture. The page utilizes JavaScript
-              functions to create user interaction.
-            </p>
-            <div className="mt-5">
+            <div className="btn mt-3">
               <a
                 href="https://dreamy-heisenberg-db9660.netlify.app/"
                 className="architecture-link"
@@ -32,21 +23,22 @@ export default function Projects() {
                 View the Page
               </a>
             </div>
+            <p className="mt-3">Technologies used:</p>
+            <img
+              src="images/Basics.png"
+              alt="Basics"
+              className="basics-image img-fluid rounded"
+            />
           </div>
-        </div>
 
-        <div className="row mt-5">
-          <div className="col-md-6">
-            <h2>Weather App</h2>
-            <p className="mt-3">
-              I built a Vanilla JavaScript weather app in SheCodes Plus, an
-              advanced 8-week JavaScript coding workshop. Building upon my
-              previously-gained knowledge, I also became skilled in Bootstrap,
-              API's, real-life development workflow, hosting, and advanced
-              JavaScript. I combined all of these skills to create a
-              fully-functioning weather app.
-            </p>
-            <div className="mt-5 mb-5">
+          <div className="col-sm">
+            <h2>Weather Application</h2>
+            <img
+              src="images/Weather.png"
+              alt="Weather project"
+              className="weather-image img-fluid rounded"
+            />
+            <div className="btn mt-3">
               <a
                 href="https://heuristic-carson-e239b2.netlify.app/"
                 className="weather-link"
@@ -57,58 +49,50 @@ export default function Projects() {
                 View the Page
               </a>
             </div>
-          </div>
-          <div className="col-md-6">
+            <p className="mt-3">Technologies used:</p>
             <img
-              src="images/Weather.png"
-              alt="Weather project"
-              className="weather-image img-fluid rounded"
+              src="images/Plus.png"
+              alt="Plus"
+              className="plus-image img-fluid rounded"
             />
           </div>
-        </div>
 
-        <div className="row mt-5">
-          <div className="col-md-6">
+          <div className="col-sm">
+            <h2>Restaurant Website</h2>
             <img
               src="images/Restaurant.png"
               alt="Loving Hut Website"
               className="restaurant-image img-fluid rounded"
             />
-          </div>
-          <div className="col-md-6">
-            <h2>Restaurant Website</h2>
-            <p className="mt-3">
-              RESPONSIVE
-              <br />I entered the world of front-end development by gaining some
-              knowledge in HTML, CSS, and JavaScript. I used my knowledge of
-              these languages, along with my new skills in VS Code, to build a
-              landing page about Architecture. The page utilizes JavaScript
-              functions to create user interaction.
-            </p>
-            <div className="mt-5">
+            <div className="btn mt-3">
               <a
                 href="https://sleepy-beaver-bd36a7.netlify.app/"
                 className="restaurant-link"
-                title="Architecture project"
+                title="Loving Hut Website"
                 target="_blank"
                 rel="noreferrer"
               >
                 View the Page
               </a>
             </div>
+            <p className="mt-3">Technologies used:</p>
+            <img
+              src="images/Responsive.png"
+              alt="Responsive"
+              className="responsive-image img-fluid rounded"
+            />
           </div>
         </div>
 
-        <div className="row mt-5">
-          <div className="col-md-6">
-            <h2>React Weather App</h2>
-            <p className="mt-3">
-              I immersed myself in React development in SheCodes React. I used
-              my knowledge of this highly-demanded framework to build a working
-              dictionary app incorporating coding best practices, along with the
-              most popular tools and technologies.
-            </p>
-            <div className="mt-5 mb-5">
+        <div className="row">
+          <div className="col-sm">
+            <h2>Weather App (React)</h2>
+            <img
+              src="images/React Weather.png"
+              alt="React Weather App"
+              className="react-weather-image img-fluid rounded"
+            />
+            <div className="btn mt-3">
               <a
                 href="https://nostalgic-brattain-8ba926.netlify.app/"
                 className="react-weather-link"
@@ -119,33 +103,22 @@ export default function Projects() {
                 View the Page
               </a>
             </div>
-          </div>
-          <div className="col-md-6">
+            <p className="mt-3">Technologies used:</p>
             <img
-              src="images/React Weather.png"
-              alt="React Weather App"
-              className="react-weather-image img-fluid rounded"
+              src="images/React.png"
+              alt="React"
+              className="react-image img-fluid rounded"
             />
           </div>
-        </div>
 
-        <div className="row mt-5">
-          <div className="col-md-6">
+          <div className="col-sm">
+            <h2>Dictionary App (React)</h2>
             <img
               src="images/React Dictionary.png"
               alt="React Dictionary App"
               className="react-dictionary-image img-fluid rounded"
             />
-          </div>
-          <div className="col-md-6">
-            <h2>React Dictionary App</h2>
-            <p className="mt-3">
-              I immersed myself in React development in SheCodes React. I used
-              my knowledge of this highly-demanded framework to build a working
-              dictionary app incorporating coding best practices, along with the
-              most popular tools and technologies.
-            </p>
-            <div className="mt-5">
+            <div className="btn mt-3">
               <a
                 href="https://pedantic-liskov-c3f65c.netlify.app/"
                 className="react-dictionary-link"
@@ -156,6 +129,12 @@ export default function Projects() {
                 View the Page
               </a>
             </div>
+            <p className="mt-3">Technologies used:</p>
+            <img
+              src="images/React.png"
+              alt="React"
+              className="react-image img-fluid rounded"
+            />
           </div>
         </div>
       </div>
