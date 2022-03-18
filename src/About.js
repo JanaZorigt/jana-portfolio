99508061d84f1647ed0ff68e920315aa7e44f0ae
JanaZorigt <91855087+JanaZorigt@users.noreply.github.com>
2022-03-17 20:07:20 -0400

    Updated tech images

--- src/About.js
+++ src/About.js
@@ -6,20 +6,20 @@ export default function About() {
       <div className="row">
         <div className="col">
           <h1>Jana Zorigt</h1>
-          <h2 className="p-3">
+          <h2>
             Front-end developer, <br />
             based in the United States
           </h2>
           <div className="mt-5 p-3">
             <p className="about-paragraph">
-              Hi, my name is Jana. I recently finished SheCodes workshops with
+              Hi! My name is Jana. I recently finished SheCodes workshops with
               great satisfaction and knowledge. Craving to learn more, I'm
               currently seeking new opportunities in front-end development. I'm
-              open to work remotely and have flexible hours. I also have
-              Finance/Accounting background (bachelor's degree). I'm very
-              fast-learner, organized, detail-oriented, responsible, and most of
-              all greatly Passionate about Coding! Please feel free to contact
-              me!
+              open to work remotely and have flexible hours. I have bachelor's
+              degree in Finance/Accounting. I'm very fast-learner, organized,
+              detail-oriented, responsible, and most of all greatly passionate
+              about coding! <br />
+              Please feel free to contact me!
             </p>
           </div>
         </div>
