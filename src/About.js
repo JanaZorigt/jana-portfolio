29f1d37a06b4629a81e94c839c55731332dcf9b9
JanaZorigt <91855087+JanaZorigt@users.noreply.github.com>
2022-03-21 00:49:16 -0400

    Real final touch

--- src/About.js
+++ src/About.js
@@ -12,8 +12,8 @@ export default function About() {
           </h2>
           <div className="mt-5 p-3">
             <p className="about-paragraph">
-              Hi! Thank you for visiting my portfolio website. My name is Jana.
-              I recently graduated as a React Developer at {""}
+              Hi! Thank you for visiting my portfolio website. My name is
+              Jana. I recently graduated as a React Developer at {""}
               <a
                 href="https://www.shecodes.io/"
                 className="shecodes-link"
@@ -25,10 +25,10 @@ export default function About() {
               </a>{" "}
               after discovering my passion for coding. I can proudly say I'm
               organized, detail-oriented, responsible and very fast-learner. My
-              background in Finance/Accounting can be a plus point in any field.{" "}
-              <br /> I'm looking forward to start a career as a Front-end
-              Developer and be of assistance in the Tech World! Check out my
-              work below and feel free to contact me.
+              background in Finance/Accounting can be a plus point in any field.
+              I'm looking forward to start a career as a Front-end Developer and
+              be of assistance in the Tech World! Check out my work below and
+              feel free to contact me.
             </p>
           </div>
         </div>
