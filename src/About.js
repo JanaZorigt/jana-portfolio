bb6f65a7d5dd55d7ccd4fb57acf8771b248f05d1
Jana Zorigt <janazorigt@gmail.com>
2022-12-07 16:13:58 -0500

    Update About.js

--- src/About.js
+++ src/About.js
@@ -13,7 +13,7 @@ export default function About() {
           <div className="mt-5 p-3">
             <p className="about-paragraph">
               Hi! Thank you for visiting my portfolio website. My name is
-              Jana. I recently graduated as a React Developer at {""}
+              Jana. After discovering my passion for coding, I graduated as a React Developer at {""}
               <a
                 href="https://www.shecodes.io/"
                 className="shecodes-link"
@@ -23,12 +23,10 @@ export default function About() {
               >
                 SheCodes
               </a>{" "}
-              after discovering my passion for coding. I can proudly say I'm
-              organized, detail-oriented, responsible and very fast-learner. My
-              background in Finance/Accounting can be a plus point in any field.
-              I'm looking forward to start a career as a Front-end Developer and
-              be of assistance in the Tech World! Check out my work below and
-              feel free to contact me.
+              and currently studying Software Engineering at Arizona State University.
+              I can proudly say I'm organized, detail-oriented, responsible and very fast-learner.
+              I'm looking forward to start my career and be of assistance in the Tech World!
+              Check out my work below and feel free to contact me.
             </p>
           </div>
         </div>
